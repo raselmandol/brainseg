941919ab3acab862a5f8bd8a32c4c6e95e17f5d0
raselmandol <raselmandol@yahoo.com>
2025-12-04 23:30:02 +0530

    version update (1.4.0)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="brainseg",
-    version="1.3.1",
+    version="1.4.0",
     description="Brain abnormality segmentation application",
     author="Md. Rasel Mandol",
     packages=find_packages(),
