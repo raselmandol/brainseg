01bce76370344f0d3148d411e3882f3fda63be6a
raselmandol <raselmandol@yahoo.com>
2026-01-03 22:25:40 +0600

    version update:stable dumb 1.5.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="brainseg-ai",
-    version="1.5.2",
+    version="1.5.5",
     description="Brain abnormality segmentation application",
     author="Md. Rasel Mandol",
     packages=find_packages(),
