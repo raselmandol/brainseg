60239c44f36fae96c5138a1c628b7ce775ec8d79
raselmandol <raselmandol@yahoo.com>
2026-04-05 21:36:10 +0530

    remove build guide

--- brainseg/help_window.py
+++ brainseg/help_window.py
@@ -120,11 +120,6 @@ class HelpWindow(QtWidgets.QDialog):
         <h3>Step 5: Save Results</h3>
         <p>Use <b>"Save Mask"</b> (Ctrl+Alt+S) or <b>"Save Highlight"</b> to save the processed images.</p>
         <p>Output files are automatically named: <code>originalname_mask.png</code> and <code>originalname_highlight.png</code></p>
-
-        <h3>Build an Executable</h3>
-        <p>To package the application as a standalone executable with PyInstaller, run the following command from the project root:</p>
-    <pre style="padding: 10px; border: 1px solid #707070; border-radius: 4px; background: transparent;"><code>pyinstaller --name brainseg-base --onefile --windowed --icon=icon.ico --add-data "brainseg\\assets;brainseg\\assets"</code></pre>
-        <p>The produced binary will appear in the <code>dist/</code> folder. Make sure the virtual environment includes all dependencies before building.</p>
         
         <h3>Navigation Tips</h3>
         <ul>
